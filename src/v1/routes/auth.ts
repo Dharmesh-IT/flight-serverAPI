dff040ebf25971d84f2c121560c0e764e9c0f32e
Dharmesh Sharma <info@dharmeshsharma.com>
2025-02-22 19:20:53 +0530

    refactor: update import statement for AuthController in auth routes

--- src/v1/routes/auth.ts
+++ src/v1/routes/auth.ts
@@ -1,5 +1,5 @@
 import Router, { Request, Response } from "express";
-import { AuthController } from "../../controllers/auth.Controller";
+
 import { container } from '../../config/inversify.config';
 import { IAuthController } from "../../controllers/Iauth.Controller";
 
